a03dec39950064bf1fcdb5f04df6b0625875a1bf
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-01-20 10:58:52 +0100

    DOM References added

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+const API_URL: string = 'https://jsonplaceholder.typicode.com/posts';
+let currentPage: number = 1;
+const itemsPerPage: number = 10;
+
+const httpClientSelectorEl = document.getElementById('http-client') as HTMLSelectElement;
+const searchInputEl = document.getElementById('search-input') as HTMLInputElement;
+const form = document.getElementById('http-client-form') as HTMLFormElement;
+
+const responseStatusContainerEl = document.getElementById('response-status-container') as HTMLDivElement;
+const dynamicDataContainerEl = document.getElementById('dynamic-data-container') as HTMLDivElement;

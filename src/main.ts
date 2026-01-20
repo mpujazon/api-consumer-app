5c1cec03a8be60bee32208f6a180b11b45665e11
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-01-20 11:16:31 +0100

    showLoading() Function Implemented and Loading Icon added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const itemsPerPage: number = 10;
 const httpClientSelectorEl = document.getElementById('http-client') as HTMLSelectElement;
 const searchInputEl = document.getElementById('search-input') as HTMLInputElement;
 const form = document.getElementById('http-client-form') as HTMLFormElement;
+const loadingElement = document.getElementById('loading-icon') as HTMLImageElement;
 
 const responseStatusContainerEl = document.getElementById('response-status-container') as HTMLDivElement;
 const dynamicDataContainerEl = document.getElementById('dynamic-data-container') as HTMLDivElement;
@@ -12,3 +13,7 @@ const dynamicDataContainerEl = document.getElementById('dynamic-data-container')
 form.addEventListener('submit', (e: Event)=> {
     e.preventDefault();
 });
+
+const showLoading = (): void => {
+    loadingElement.classList.remove('hidden');
+}
